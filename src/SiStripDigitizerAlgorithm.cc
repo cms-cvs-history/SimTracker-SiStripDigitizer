60eac08b127f4589a5b0a506674b4482e44b076c
cmsbuild <>
2007-02-23 15:31:24 +0000

    This commit was manufactured by cvs2git to create tag 'SimTracker- SiStripDigitizer-V03-03-09'.
    Sprout from master 2007-02-05 11:37:18 UTC fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch> 'fix namespace'
    Cherrypick from master 2007-02-23 15:31:23 UTC Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch> 'CouplingCostant changed from 0.76,0.12 to 0.86,0.07':
        data/SiStripDigiMTCC.cfi
        src/SiStripDigitizerAlgorithm.cc

--- src/SiStripDigitizerAlgorithm.cc
+++ src/SiStripDigitizerAlgorithm.cc
@@ -27,7 +27,8 @@
 #define e_SI (1.6E-19)
 
 SiStripDigitizerAlgorithm::SiStripDigitizerAlgorithm(const edm::ParameterSet& conf, StripGeomDetUnit *det,
-						     uint32_t& idForNoise , SiStripNoiseService* noiseService):conf_(conf){
+						     uint32_t& idForNoise , SiStripNoiseService* noiseService,const ParticleDataTable * pdt):conf_(conf),
+																       pdt_(pdt){
   //  cout << "Creating a SiStripDigitizerAlgorithm." << endl;
 
   ndigis=0;
@@ -58,9 +59,15 @@ SiStripDigitizerAlgorithm::SiStripDigitizerAlgorithm(const edm::ParameterSet& co
   int strip = int(numStrips/2.);
   float noiseRMS = SiStripNoiseService_->getNoise(idForNoise,strip);
 
+<<<<<<< SiStripDigitizerAlgorithm.cc
+  theSiNoiseAdder = new SiGaussianTailNoiseAdder(numStrips,noiseRMS*theElectronPerADC,theThreshold);
+  theSiZeroSuppress = new SiTrivialZeroSuppress(conf_,noiseRMS);
+  theSiHitDigitizer = new SiHitDigitizer(conf_,det);
+=======
   theSiNoiseAdder = new SiGaussianTailNoiseAdder(numStrips,noiseRMS,theThreshold);
   theSiZeroSuppress = new SiTrivialZeroSuppress(conf_,noiseRMS/theElectronPerADC);
-  theSiHitDigitizer = new SiHitDigitizer(conf_,det);
+  theSiHitDigitizer = new SiHitDigitizer(conf_,det,pdt_);
+>>>>>>> 1.21
   theSiPileUpSignals = new SiPileUpSignals();
   theSiDigitalConverter = new SiTrivialDigitalConverter(theElectronPerADC,theAdcFullScale);
 

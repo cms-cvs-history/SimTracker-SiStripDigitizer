a8b7abdb8efe989806f5794a336ff8b8de7bb6c4
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-04-06 22:08:31 +0000

    Initial changes for pileup memory reduction

--- interface/SiPileUpSignals.h
+++ interface/SiPileUpSignals.h
@@ -2,11 +2,12 @@
 #define Tracker_SiPileUpSignals_h
 
 #include <map>
+#include <memory>
 #include <vector>
-#include "SimDataFormats/TrackingHit/interface/PSimHit.h"
 #include "FWCore/Utilities/interface/Map.h"
+#include "SimTracker/Common/interface/SimHitInfoForLinks.h"
 
-class SimHit;
+class PSimHit;
 /**
  * Class which takes the responses from each SimHit and piles-up them, within a given module.
  * More precisely, it keeps for each strip the link to each individual measurement.
@@ -15,10 +16,10 @@ class SiPileUpSignals{
  public:
   // type used to describe the amplitude on a strip
   typedef float Amplitude;
-  typedef std::map<unsigned int, Amplitude> ChannelMapType;
-  // associates to each strip a vector of amplitudes. 
-  typedef std::map<int, ChannelMapType>  SignalMapType;
-  typedef std::map<uint32_t, SignalMapType> signalMaps;
+  // associates to each strip a vector of pairs {simhit,amplitude}.
+  // That allows later to comput the fraction of the contribution of each simhit to the ADC value
+  typedef std::map<int, std::vector<std::pair<std::shared_ptr<SimHitInfoForLinks>, Amplitude> >, std::less<int> >  HitToDigisMapType;
+  typedef std::map<uint32_t, HitToDigisMapType> signalMaps;
   
   SiPileUpSignals() { reset(); }
 
@@ -28,11 +29,11 @@ class SiPileUpSignals{
                    const std::vector<double>& locAmpl,
                    const size_t& firstChannelWithSignal,
                    const size_t& lastChannelWithSignal,
-                   unsigned int trackID);
+                   const PSimHit* hit);
 
   void reset(){ resetSignals(); }
   
-  const SignalMapType* getSignal(uint32_t detID) const {
+  const HitToDigisMapType* getSignal(uint32_t detID) const {
     auto where = signal_.find(detID);
     if(where == signal_.end()) {
       return 0;
--- src/SiPileUpSignals.cc
+++ src/SiPileUpSignals.cc
@@ -8,12 +8,9 @@ void SiPileUpSignals::resetSignals(){
 void SiPileUpSignals::add(uint32_t detID, const std::vector<double>& locAmpl,
                           const size_t& firstChannelWithSignal,
                           const size_t& lastChannelWithSignal,
-                          unsigned int trackID) {
-  SignalMapType& theSignal = signal_[detID];
-  for (size_t iChannel=firstChannelWithSignal; iChannel<lastChannelWithSignal; ++iChannel) {
-    if(locAmpl[iChannel] != 0.0) {
-      ChannelMapType& theChannel = theSignal[iChannel];
-      theChannel[trackID] += locAmpl[iChannel];
-    }
+                          const PSimHit* hit) {
+  HitToDigisMapType& theMapLink = signal_[detID];
+  for(size_t iChannel = firstChannelWithSignal; iChannel<lastChannelWithSignal; ++iChannel) {
+    theMapLink[iChannel].emplace_back(std::shared_ptr<SimHitInfoForLinks>(new SimHitInfoForLinks(hit)), Amplitude(locAmpl[iChannel]));
   }
 }
